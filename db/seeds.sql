bcdb7314869d7f01d791d8ef4d3de8bac3aeb895
markcam1 <32405690+markcam1@users.noreply.github.com>
2018-06-05 19:01:51 -0700

    v1

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', true);
